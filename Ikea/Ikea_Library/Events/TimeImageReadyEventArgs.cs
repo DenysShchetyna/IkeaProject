29f097494139f583c6a0279f0014c1b3822a3e1d
DenysShchetyna <denisschetina@gmail.com>
2020-06-15 14:40:11 +0200

    development

---
+++ Ikea/Ikea_Library/Events/TimeImageReadyEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikea_Library.Events
+{
+    public class TileImageReadyEventArgs : EventArgs
+    {
+        public HObject TileImage { get; set; }
+        public string CamName { get; set; }
+
+        public TileImageReadyEventArgs(string camName, HObject tileImage)
+        {
+            TileImage = tileImage;
+            CamName = camName;
+        }
+    }
+}
